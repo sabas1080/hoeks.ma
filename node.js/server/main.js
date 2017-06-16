0121990d32ff829456709b5d54b9bcccae497e08
Unknown <julio.romotishca@gmail.com>
2017-06-16 17:52:39 -0500

    v1.4.0
    Working whit beelan api

--- node.js/server/main.js
+++ node.js/server/main.js
@@ -4,55 +4,71 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var mysql = require('mysql');
 var gps = require('wifi-location');
-var ttn = require('ttn');
+//beelan
+var ttn = require('ttn'),
+  path = require('path');
+var fetch = require('node-fetch');
+var atob = require('atob');
+var math = require('mathjs');
+var convertHex = require('convert-hex')
+//------------------------
 var sock = null;
 var appId = 'tishcatrack'; // INSERT TTN YOUR AppEUI
 var accessKey = 'ttn-account-v2.NuxDXbvuSyi0SJlXY0N5IOcZYyEgiyRS-btAukllQWI'; // INSERT TTN accessKey
 var client = new ttn.Client('eu', appId, accessKey);
-//------------------ ( TTN ) ---------------------------
-client.on('connect', function() {
-  console.log('Conectado a TTN');
-});
-client.on('message', function(deviceId, msg) {
-  var formattedData = JSON.parse(JSON.stringify(msg, null, 2))
-  formattedData.payload_raw = formattedData.payload_raw.data;
-  console.info(formattedData.payload_raw);
-  console.log();
-  console.log("length " + formattedData.payload_raw.length);
-  var mac = [];
-  var rssi = [];
-  var tmp = "";
-  for (i = 0; i < formattedData.payload_raw.length; i++) {
-    if ((i + 1) % 7 == 0) {
-      tmp = tmp.slice(0, -1);
-      rssi = parseInt(formattedData.payload_raw[i]);
-      console.log("mac " + tmp + " rssi " + rssi);
-      mac.push({
-        mac: tmp,
-        ssid: '',
-        signal_level: "-" + rssi
-      });
-      i++;
-      tmp = "";
-    }
-    var hex = parseInt(formattedData.payload_raw[i]).toString(16);
-    tmp = tmp + hex + ":";
+//----------------Api beelan----------------------------
+const url = 'http://web.beelan.mx/api/upLink/1111111111111111';
+var SendSocket = []
+const getData = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => response.slice(Math.max(response.length - 1, 1))) //-10
+    .then((response) => parseData(response))
+    //  .then((response) => console.log(response)) //-10
+    .then((response) => SendSocket = response) //-10
+
+    //.then((response) => console.log(response)) //-10
+    .catch((err) => console.log(err));
+}
+
+const parseData = (data) => {
+  // la data de cada uno de los objetos sera convertida de base64 a hexa y de hexa a decimal
+  return data.map((item) => {
+    //console.log(base64toHEX(item.data))
+    let wero = {};
+    wero.data = parseHexString(base64toHEX(item.data));
+    return wero;
+  })
+}
+const base64toHEX = (base64) => {
+  const raw = atob(base64);
+  let HEX = '';
+  for (let i = 0; i < raw.length; i++) {
+    const hex = raw.charCodeAt(i).toString(16);
+    HEX += (hex.length === 2 ? hex : `0${hex}`);
   }
-  getLocation(mac);
-});
+  return HEX.toUpperCase();
+}
 
-client.on('activation', function(evt) {
-  console.log("activation");
-});
+function parseHexString(str) {
 
-client.on('error', function(err) {
-  console.error('[ERROR]', err.message);
-});
+  var result = [];
+  while (str.length >= 2) {
+    result.push(parseInt(str.substring(0, 2), 16));
+    str = str.substring(2, str.length);
 
-function setSocket(s) {
-  sock = s;
-  console.log("socket");
-  console.log(s);
+  }
+  //console.log(result);
+  return result;
+}
+
+function corrimiento(data) {
+  pos = {
+    lat: (data[4] + (data[3] << 8) + (data[2] << 16)) / 10000,
+    lng: (-1) * ((data[7] + (data[6] << 8) + (data[5] << 16)) / 10000),
+    //alt: ((data[10] + (data[9] << 8) + (data[8] <<16 )) / 100)
+  };
+  return pos;
 }
 
 function getLocation(plop) {
@@ -64,11 +80,51 @@ function getLocation(plop) {
     console.log(loc.latitude + ", " + loc.longitude);
     console.log("error: " + err);
     var d = new Date();
-    console.log(' Hora '+d.getHours()+":"+d.getMinutes()+"");
-    io.sockets.emit('location', {latitu:loc.latitude,longi:loc.longitude});
+    console.log(' Hora ' + d.getHours() + ":" + d.getMinutes() + "");
+    io.sockets.emit('location', {
+      latitu: loc.latitude,
+      longi: loc.longitude
+    });
 
   });
 }
+const runArray = (data) => {
+  return data.map((item) => {
+    return parseHexString(item.data);
+  })
+}
+//------------------ ( TTN ) ---------------------------
+client.on('connect', function() {
+  console.log('Conectado a TTN');
+});
+
+setInterval(() => {
+  getData();
+  if (SendSocket.length > 0) {
+    console.log(SendSocket[0].data);
+    console.log("length " + SendSocket[0].data.length);
+    var mac = [];
+    var rssi = [];
+    var tmp = "";
+    for (i = 0; i < SendSocket[0].data.length; i++) {
+      if ((i + 1) % 7 == 0) {
+        tmp = tmp.slice(0, -1);
+        rssi = parseInt(SendSocket[0].data[i]);
+        console.log("mac " + tmp + " rssi " + rssi);
+        mac.push({
+          mac: tmp,
+          ssid: '',
+          signal_level: "-" + rssi
+        });
+        i++;
+        tmp = "";
+      }
+      var hex = parseInt(SendSocket[0].data[i]).toString(16);
+      tmp = tmp + hex + ":";
+    }
+    getLocation(mac);
+  }
+}, 10000); //
 
 //------------------------------------------------------
 app.use(express.static('public'));
@@ -94,11 +150,9 @@ connection.connect(function(error) {
 //-------------------------( SOCKETS)------------------------------------
 io.sockets.on('connection', function(socket) {
   console.log("Usuario Conectado por sockets");
-  socket.on('messageChange', function(data) {
-    console.log(data);
 
-  });
 });
+
 
 //---------------------(SERVER)-----------------------
 
---
+++ node.js/server/main0.js
@@ -0,0 +1,150 @@
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+const fs = require('fs');
+
+var fetch = require('node-fetch');
+var atob = require('atob');
+var Highcharts = require('highcharts');
+
+var math = require('mathjs');
+var convertHex = require('convert-hex')
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+//var mqtt = require('mqtt');
+//var client  = mqtt.connect('http://198.199.97.15', {username:'master', password:'f4rm1nGC'})
+
+var pos;
+var lng,lat;
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/public', routes.index);
+app.get('/users', user.list);
+
+const url = 'http://web.beelan.mx/api/upLink/43435daeccbb21b0';
+    // ------------------------------------------------------------  //
+    var SendSocket = []
+const getData = () => {
+      fetch(url)
+        .then((response) => response.json())
+        //.then((response) => parseData(response.slice(Math.max(response.length-1, 1)))) //-10
+        .then((response) => parseData(response)) //-10
+        .then((response) => SendSocket = response) //-10
+        //.then((response) => console.log(response)) //-10
+        .catch((err) => console.log(err));
+    }
+
+// La data de cada uno de los paquetes recividos esta en base64
+const parseData = (data) => {
+  // la data de cada uno de los objetos sera convertida de base64 a hexa y de hexa a decimal
+  return data.map((item) => {
+    //console.log(base64toHEX(item.data))
+    let wero = {};
+     wero.data = corrimiento(parseHexString(base64toHEX(item.data)));
+      wero.color = colores(item.macGateway);
+      wero.fCnt = item.fCnt;
+     return wero;
+  })
+}
+
+const runArray = (data) => {
+  return data.map((item) => {
+    return parseHexString(item.data);
+  })
+}
+const base64toHEX = (base64) => {
+  const raw = atob(base64);
+  let HEX = '';
+  for (let i = 0; i < raw.length; i++) {
+    const hex = raw.charCodeAt(i).toString(16);
+    HEX += (hex.length === 2 ? hex : `0${hex}`);
+  }
+  return HEX.toUpperCase();
+}
+
+io.sockets.on('connection', function(socket){
+	socket.on('coords:me', function(data){
+		//console.log(data);
+		socket.broadcast.emit('coords:user', data);
+	});
+  setInterval(() => {
+    getData()
+    //console.log(SendSocket)
+    socket.emit('coords:gps', {
+           latlng: SendSocket
+        });
+  }, 10000);
+
+});
+
+function parseHexString(str) {
+  console.log(str);
+    var result = [];
+    while (str.length >= 2) {
+        result.push(parseInt(str.substring(0, 2), 16));
+        str = str.substring(2, str.length);
+
+    }
+    console.log(result);
+    return result;
+}
+
+function colores(gateway) {
+  switch (gateway) {
+    case '008000000000aaaa':
+      return '#f51035'
+      break;
+    case '009000000000aabb':
+        return '#bafc5c'
+        break;
+    case 'b827ebffffbe86a6':
+        return '#ffae34'
+        break;
+    default:
+
+  }
+
+}
+
+function corrimiento(data) {
+    pos = {
+       lat: (data[4] + (data[3] << 8) + (data[2] <<16 )) / 10000,
+       lng: (-1)*((data[7] + (data[6] << 8) + (data[5] <<16 )) / 10000),
+       //alt: ((data[10] + (data[9] << 8) + (data[8] <<16 )) / 100)
+    };
+    return pos;
+}
+
+function parseHexString(str) {
+    var result = [];
+    while (str.length >= 2) {
+        result.push(parseInt(str.substring(0, 2), 16));
+        str = str.substring(2, str.length);
+    }
+    return result;
+}
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
